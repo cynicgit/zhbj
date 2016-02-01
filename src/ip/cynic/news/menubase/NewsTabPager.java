9089b88e35c7a6f19923632be5fc685f9272c087
957160415@qq.com <957160415@qq.com>
2016-02-01 18:04:28 +0800

    下拉刷新 加载更多

--- src/ip/cynic/news/menubase/NewsTabPager.java
+++ src/ip/cynic/news/menubase/NewsTabPager.java
@@ -8,15 +8,22 @@ import ip.cynic.news.domain.NewsDetailData;
 import ip.cynic.news.domain.NewsDetailData.News;
 import ip.cynic.news.domain.NewsDetailData.TopNews;
 import ip.cynic.news.global.URL;
+import ip.cynic.news.view.NewsListView;
+import ip.cynic.news.view.NewsListView.RefreshListener;
+import ip.cynic.xmpp.utils.ToastUtils;
 import android.app.Activity;
+import android.location.GpsStatus.NmeaListener;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -28,7 +35,7 @@ import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
 import com.viewpagerindicator.CirclePageIndicator;
 
-public class NewsTabPager extends BaseMenuDetailPager implements OnPageChangeListener{
+public class NewsTabPager extends BaseMenuDetailPager implements OnPageChangeListener {
 
 	public NewsTabData newsTabData;
 	private NewsDetailData mNewsDetailData;
@@ -36,8 +43,10 @@ public class NewsTabPager extends BaseMenuDetailPager implements OnPageChangeLis
 
 	private CirclePageIndicator indicator;
 	private TextView mTopNewsTitle;
-	private List<TopNews> topnews;
-	private List<News> news;
+	private List<TopNews> mTopnews;
+	private List<News> mNews;
+	private NewsListView mLvNews;
+	private String mMoreUrl;
 
 	public NewsTabPager(Activity activity, NewsTabData newsTabData) {
 		super(activity);
@@ -47,9 +56,31 @@ public class NewsTabPager extends BaseMenuDetailPager implements OnPageChangeLis
 	@Override
 	public View initView() {
 		View view = View.inflate(mActivity, R.layout.activity_news_tab_content, null);
-		mVpTopNews = (ViewPager) view.findViewById(R.id.vp_center);
-		indicator = (CirclePageIndicator) view.findViewById(R.id.indicator);
-		mTopNewsTitle = (TextView) view.findViewById(R.id.tv_topnews_title);
+		mLvNews = (NewsListView) view.findViewById(R.id.lv_news_detail);
+		
+		View headerView = View.inflate(mActivity, R.layout.activity_top_news, null);
+		
+		mVpTopNews = (ViewPager) headerView.findViewById(R.id.vp_center);
+		indicator = (CirclePageIndicator) headerView.findViewById(R.id.indicator);
+		mTopNewsTitle = (TextView) headerView.findViewById(R.id.tv_topnews_title);
+		mLvNews.addHeaderView(headerView);
+		
+		mLvNews.setOnRefreshListener(new RefreshListener() {
+			@Override
+			public void onRefresh() {
+				getNewsDataByServer();
+			}
+
+			@Override
+			public void onLoadMore() {
+				if(mMoreUrl!=null){
+					getNewsMoreDataByServer();
+				}else{
+					ToastUtils.showToastSafe(mActivity, "最后一页");
+					mLvNews.refreshCompleted(false);
+				}
+			}
+		});
 		return view;
 	}
 
@@ -59,40 +90,70 @@ public class NewsTabPager extends BaseMenuDetailPager implements OnPageChangeLis
 	}
 
 	public void getNewsDataByServer() {
-
 		HttpUtils httpUtils = new HttpUtils(15000);
 		httpUtils.send(HttpMethod.GET, URL.BASE_URL + newsTabData.url, new RequestCallBack<String>() {
 			@Override
 			public void onSuccess(ResponseInfo<String> responseInfo) {
-
 				String result = responseInfo.result;
-				// System.out.println("json:" + result);
+				System.out.println("json:" + result);
 				parseData(result);
+				mLvNews.refreshCompleted(true);
 			}
 
 			@Override
 			public void onFailure(HttpException error, String msg) {
 				Log.e("HttpUtils", msg);
 				error.printStackTrace();
+				mLvNews.refreshCompleted(false);
 			}
 
 		});
 
 	}
+	
+	public void getNewsMoreDataByServer() {
+		HttpUtils httpUtils = new HttpUtils(15000);
+		httpUtils.send(HttpMethod.GET, URL.BASE_URL + newsTabData.url, new RequestCallBack<String>() {
+			@Override
+			public void onSuccess(ResponseInfo<String> responseInfo) {
+				String result = responseInfo.result;
+				System.out.println("json:" + result);
+				parseData(result);
+				mLvNews.refreshCompleted(true);
+			}
+			
+			@Override
+			public void onFailure(HttpException error, String msg) {
+				Log.e("HttpUtils", msg);
+				error.printStackTrace();
+				mLvNews.refreshCompleted(false);
+			}
+			
+		});
+		
+	}
 
 	protected void parseData(String result) {
 		Gson gson = new Gson();
 		mNewsDetailData = gson.fromJson(result, NewsDetailData.class);
-		topnews = mNewsDetailData.data.topnews;
-		news = mNewsDetailData.data.news;
-		
+		mTopnews = mNewsDetailData.data.topnews;
+		String url = mNewsDetailData.data.more;
+		if(!TextUtils.isEmpty(url)){
+			mMoreUrl = URL.BASE_URL + url;
+		}else{
+			mMoreUrl = null;
+		}
+
+		mNews = mNewsDetailData.data.news;
 		mVpTopNews.setAdapter(new MyAdapter());
 		indicator.setViewPager(mVpTopNews);
 		indicator.setSnap(true);
 		indicator.onPageSelected(0);
 		indicator.setOnPageChangeListener(this);
+
+		mTopNewsTitle.setText(mTopnews.get(0).title);
 		
-		mTopNewsTitle.setText(topnews.get(0).title);
+		mLvNews.setAdapter(new MyListAdapter());
 	}
 
 	class MyAdapter extends PagerAdapter {
@@ -105,7 +166,7 @@ public class NewsTabPager extends BaseMenuDetailPager implements OnPageChangeLis
 
 		@Override
 		public int getCount() {
-			return topnews.size();
+			return mTopnews.size();
 		}
 
 		@Override
@@ -117,7 +178,7 @@ public class NewsTabPager extends BaseMenuDetailPager implements OnPageChangeLis
 		public Object instantiateItem(ViewGroup container, int position) {
 			ImageView imgView = new ImageView(mActivity);
 			imgView.setScaleType(ScaleType.FIT_XY);
-			TopNews news = topnews.get(position);
+			TopNews news = mTopnews.get(position);
 			System.out.println(news.topimage);
 			bitmapUtils.display(imgView, news.topimage);
 			container.addView(imgView);
@@ -131,13 +192,71 @@ public class NewsTabPager extends BaseMenuDetailPager implements OnPageChangeLis
 
 	}
 
+	class MyListAdapter extends BaseAdapter {
+
+		private BitmapUtils bitmapUtils;
+
+		public MyListAdapter(){
+			bitmapUtils = new BitmapUtils(mActivity);
+			bitmapUtils.configDefaultLoadFailedImage(R.drawable.pic_item_list_default);
+		}
+		
+		@Override
+		public int getCount() {
+			return mNews.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return mNews.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			View view = null;
+			ViewHodler hodler = null;
+			if (convertView == null) {
+				view = View.inflate(mActivity, R.layout.item_news_list, null);
+				hodler = new ViewHodler();
+				hodler.ivNews = (ImageView) view.findViewById(R.id.iv_news);
+				hodler.tvTitle = (TextView) view.findViewById(R.id.tv_title);
+				hodler.tvTime = (TextView) view.findViewById(R.id.tv_time);
+				view.setTag(hodler);
+			} else {
+				view = convertView;
+				hodler = (ViewHodler) view.getTag();
+			}
+			
+			News news = mNews.get(position);
+			
+			bitmapUtils.display(hodler.ivNews, news.listimage);
+			hodler.tvTitle.setText(news.title);
+			hodler.tvTime.setText(news.pubdate);
+			
+			return view;
+		}
+
+	}
+
+	static class ViewHodler {
+		ImageView ivNews;
+		TextView tvTitle;
+		TextView tvTime;
+	}
+
 	@Override
 	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 	}
 
 	@Override
 	public void onPageSelected(int position) {
-		String title = topnews.get(position).title;
+		String title = mTopnews.get(position).title;
 		mTopNewsTitle.setText(title);
 	}
 
